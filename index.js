a6f111005bcc51ba87a67f663cca8afe8b0424fa
Andrew McFadden <mcfadden.andrewj@gmail.com>
2023-08-29 14:17:03 -0600

    modifying api call

--- index.js
+++ index.js
@@ -1,35 +1,37 @@
+// const { data } = require("browserslist");
+
 // DOM handling
-const img = document.querySelector("img");
+const display = document.getElementById("display");
 const searchBtn = document.getElementById("search-btn");
-const searchInput = document.getElementById("gif-search");
-const imgDefault = "start";
+const searchInput = document.getElementById("location-search");
 const searchError = document.getElementById("search-error");
-const apiBase = "https://api.giphy.com/v1/gifs/translate?api_key=5USKJ6tsZp7la5EBedWxG0Z9vjybhKFY&s=";
+const apiBase = "https://api.weatherapi.com/v1/current.json?key=1fdeb95185c145f0809201723232908&q=";
+
+// Functions to search and return gifs
+
+async function fetchWeather(location) {
+  try {
+    const response = await fetch(apiBase.concat(location), { mode: "cors" });
+    const weatherData = await response.json();
+    searchError.textContent = "";
+    return weatherData;
+  } catch (e) {
+    searchError.textContent = "Invalid search, cannot find location. Try again.";
+  }
+}
+
+function runSearch() {
+  const location = document.getElementById("location-search").value;
+  const currentWeather = fetchWeather(location);
+  console.log(currentWeather);
+}
 
 // Event handlers
 
-searchBtn.onclick = searchGif;
+searchBtn.onclick = runSearch;
 searchInput.addEventListener("keypress", (e) => {
   if (e.key === "Enter") {
     // e.preventDefault();
     searchBtn.click();
   }
 });
-
-// Functions to search and return gifs
-function searchGif(img) {
-  const gifInput = document.getElementById("gif-search").value;
-  img.src = fetchGif(apiBase, gifInput);
-}
-
-async function fetchGif(apiBase, gifInput) {
-  try {
-    const response = await fetch(apiBase.concat(gifInput), { mode: "cors" });
-    const catData = await response.json();
-    searchError.textContent = "";
-    img.src = catData.data.images.original.url;
-  } catch (e) {
-    searchError.textContent = "Invalid search, no gifs found. Try again.";
-    img.src = "#";
-  }
-}
