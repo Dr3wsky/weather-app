242881af55f531fafe662e4ca16e6f313238bf17
Andrew McFadden <mcfadden.andrewj@gmail.com>
2023-08-29 11:54:25 -0600

    updating rule dependencies

--- index.js
+++ index.js
@@ -1,34 +1,35 @@
 // DOM handling
-const img = document.querySelector('img');
-const searchBtn = document.getElementById('search-btn')
-const searchInput = document.getElementById('gif-search')
-const imgDefault = 'start';
+const img = document.querySelector("img");
+const searchBtn = document.getElementById("search-btn");
+const searchInput = document.getElementById("gif-search");
+const imgDefault = "start";
 const searchError = document.getElementById("search-error");
-const apiBase = 'https://api.giphy.com/v1/gifs/translate?api_key=5USKJ6tsZp7la5EBedWxG0Z9vjybhKFY&s=';
+const apiBase = "https://api.giphy.com/v1/gifs/translate?api_key=5USKJ6tsZp7la5EBedWxG0Z9vjybhKFY&s=";
 
 // Event handlers
+
 searchBtn.onclick = searchGif;
-searchInput.addEventListener("keypress", function(e) {
+searchInput.addEventListener("keypress", (e) => {
   if (e.key === "Enter") {
     // e.preventDefault();
     searchBtn.click();
-  };
-}); 
+  }
+});
 
 // Functions to search and return gifs
 function searchGif(img) {
-const gifInput = document.getElementById("gif-search").value;
-img.src = fetchGif(apiBase, gifInput);
+  const gifInput = document.getElementById("gif-search").value;
+  img.src = fetchGif(apiBase, gifInput);
 }
 
 async function fetchGif(apiBase, gifInput) {
   try {
-    const response =  await fetch(apiBase.concat(gifInput), {mode: 'cors'});
+    const response = await fetch(apiBase.concat(gifInput), { mode: "cors" });
     const catData = await response.json();
     searchError.textContent = "";
-    img.src =  catData.data.images.original.url;
+    img.src = catData.data.images.original.url;
   } catch (e) {
-    searchError.textContent = "Invalid search, no gifs found. Try again."
-    img.src = '#';
-  };
+    searchError.textContent = "Invalid search, no gifs found. Try again.";
+    img.src = "#";
+  }
 }
