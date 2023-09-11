57145a34e199ebacfa959d07ee2249432b1eaa5e
Andrew McFadden <mcfadden.andrewj@gmail.com>
2023-09-11 16:56:46 -0400

    modifying forecast object

--- index.js
+++ index.js
@@ -118,8 +118,9 @@ function parseData(data) {
     feelsLike: `Gusting to ${data.current.gust_kph} kph, Feels like ${data.current.feelslike_c} °C`,
   };
   
-  data.forecast.forecastday.forEach((day) => {
-    let day = {
+  for (let day=0; day<data.forecast.forecastday.length; i++) {
+    let willSnow = data.forecast.forecastday[day].daily_will_it_snow;
+    let forecast[day] = {
     date: data.forecast.forecastday[day].date, 
     condtionIcon: data.forecast.forecastday[day].day.condition.icon, 
     condition: data.forecast.forecastday[day].day.condition.text, 
@@ -128,16 +129,10 @@ function parseData(data) {
     maxTemps: `High: ${data.forecast.forecastday[day].day.maxtemp_c} °C`,
     minTemps: `Low: ${data.forecast.forecastday[day].day.mintemp_c} °C`,
     humidity: `${data.forecast.forecastday[day].day.avghumidity}% Humidity`,
-    if (data.forecast.forecastday[day].day.daily_will_it_snow) {
-      precip: `${data.forecast.forecastday[day].day.daily_chance_of_snow}% chance of snow`,
-      precipTotal: `${data.forecast.forecastday[day].day.totalsnow_cm}cm total snowfall`,
+    precip: ((willSnow)? `${data.forecast.forecastday[day].day.daily_chance_of_snow}% chance of snow`: `${data.forecast.forecastday[day].day.daily_chance_of_rain}% chance of rain`),
+    precipTotal: ((willSnow)`${data.forecast.forecastday[day].day.totalsnow_cm}cm total snowfall`: `${data.forecast.forecastday[i].day.totalprecip_mm}mm total precipitation`)
     } 
-    else {
-      precip: `${data.forecast.forecastday[i].day.daily_chance_of_rain}% chance of rain`,
-      precipTotal: `${data.forecast.forecastday[i].day.totalprecip_mm}mm total precipitation`,
-    }
-  }})
-};
+  };
   return { place, current, forecast };
 }
 
