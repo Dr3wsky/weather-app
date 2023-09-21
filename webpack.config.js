c539c05499b4391038015fc4b9533acb1d475db4
Andrew McFadden <mcfadden.andrewj@gmail.com>
2023-09-21 09:27:39 -0600

    updating dist folder to docs for puvlish

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,12 @@
 const path = require("path");
 
 module.exports = {
-  mode: "development",
+  mode: "production",
   devtool: "inline-source-map",
   entry: "./src/index.js",
   output: {
     filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "docs"),
   },
   module: {
     rules: [
